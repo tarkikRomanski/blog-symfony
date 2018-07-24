9b15d9d47fd3b15cd3e9f2d6e7ef3b7252090af1
tarkikRomanski <tarkik@ukr.net>
2018-07-24 21:43:43 +0300

    added requires & category init controller

---
+++ src/Controller/CategoryController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class CategoryController extends Controller
+{
+    /**
+     * @Route("/", name="category")
+     */
+    public function index()
+    {
+        return $this->render('category/index.html.twig');
+    }
+}
