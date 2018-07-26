3e281917ebc026e5b74a939f310fde41ca423a6e
Roman <tarkik@hadornag.ch>
2018-07-26 15:04:41 +0300

    added api controller for post

--- src/Controller/Api/PostController.php
+++ src/Controller/Api/PostController.php
@@ -4,10 +4,12 @@ namespace App\Controller\Api;
 
 use App\Entity\Category;
 use App\Entity\Post;
+use App\Service\Helper;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Serializer\SerializerInterface;
 
 /**
@@ -27,7 +29,7 @@ class PostController extends Controller
     {
         $categoryId = $request->get('category');
 
-        if(!is_null($categoryId)) {
+        if (!is_null($categoryId)) {
             $category = $this->getDoctrine()
                 ->getRepository(Category::class)
                 ->find($categoryId);
@@ -46,14 +48,65 @@ class PostController extends Controller
      * @Route("/posts", name="api.post.store", methods="POST")
      * @param Request $request
      * @param SerializerInterface $serializer
+     * @param Helper $helper
      * @return Response
      */
-    public function store(Request $request, SerializerInterface $serializer) {
+    public function store(Request $request, SerializerInterface $serializer, Helper $helper)
+    {
+        $entityManager = $this->getDoctrine()->getManager();
         $post = new Post();
-        
         $post->setName($request->get('name'));
         $post->setContent($request->get('content'));
 
+        if (!is_null($request->get('categories'))) {
+            $newCategories = $helper->toCategories($request->get('categories'));
+            foreach ($newCategories as $category) {
+                $post->addCategory($category);
+            }
+        }
+
+        $entityManager->persist($post);
+        $entityManager->flush();
+
         return new Response($serializer->serialize($post, 'json'), Response::HTTP_CREATED);
+    }
+
+    /**
+     * @Route("/posts/{id}", name="api.post.update", methods="PUT")
+     * @param int $id
+     * @param Request $request
+     * @param SerializerInterface $serializer
+     * @param Helper $helper
+     * @return Response
+     */
+    public function update($id, Request $request, SerializerInterface $serializer, Helper $helper)
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        $post = $entityManager->getRepository(Post::class)->find($id);
+        $post->setName($request->get('name'));
+        $post->setContent($request->get('content'));
+
+        if (!is_null($request->get('categories'))) {
+            $newCategories = $helper->toCategories($request->get('categories'));
+            foreach ($newCategories as $category) {
+                $post->addCategory($category);
+            }
+        }
+        $entityManager->flush();
+
+        return new Response($serializer->serialize($post, 'json'), Response::HTTP_CREATED);
+    }
+
+    /**
+     * @Route("/posts/{id}", name="api.post.delete", methods="DELETE")
+     * @param $id
+     * @return JsonResponse
+     */
+    public function delete($id) {
+        $entityManager = $this->getDoctrine()->getManager();
+        $post = $entityManager->getRepository(Post::class)->find($id);
+        $entityManager->remove($post);
+        $entityManager->flush();
+        return new JsonResponse(['id' => $id], Response::HTTP_OK);
     }
 }
